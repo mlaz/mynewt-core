e8692089ebb96396da723c1f939c15f8350de3f6
Miguel Azevedo <miguellazev@gmail.com>
2019-03-25 15:11:02 +0000

    saving progress

--- hw/drivers/1_wire/src/1_wire.c
+++ hw/drivers/1_wire/src/1_wire.c
@@ -26,7 +26,8 @@
 int
 1_wire_configure(struct 1_wire_dev *dev, uint8_t pin, bool pullup)
 {
-
+    assert(dev->1_wire_configure);
+    return dev->1_wire_configure(dev);
 }
 
 /**
@@ -36,7 +37,8 @@ int
 int
 1_wire_reset(struct 1_wire_dev *dev)
 {
-
+    assert(dev->1_wire_reset);
+    return dev->1_wire_reset(dev);
 }
 
 /**
@@ -46,7 +48,19 @@ int
 uint8_t
 1_wire_read_byte(struct 1_wire_dev *dev)
 {
+    int i;
+    uint8_t word = 0;
 
+    if (dev->1_wire_read_byte) {
+        return dev->1_wire_read_byte(dev);
+    }
+
+    assert(dev->1_wire_read_bit);
+    for (i = 1; i < 8; i++) {
+        word >>= 1;
+        word |= (dev->1_wire_read_bit(dev)) ? 0x80 : 0x00;
+    }
+    return word;
 }
 
 /**
@@ -56,7 +70,18 @@ uint8_t
 void
 1_wire_write_byte(struct 1_wire_dev *dev, uint8_t)
 {
+    int i;
 
+    if (dev->1_wire_write_byte) {
+        dev->1_wire_write_byte(dev);
+        return;
+    }
+
+    assert(dev->1_wire_write_bit);
+    for (i = 0; i < 8; i++) {
+        dev->1_wire_write_bit(dev, word & 0x01);
+        data >>= 1;
+    }
 }
 
 /**
@@ -66,7 +91,21 @@ void
 uint8_t
 1_wire_read_block(struct 1_wire_dev *dev, uint8_t *block, int len)
 {
+    if (dev->1_wire_read_block) {
+        return dev->1_wire_read_block(dev);
+    }
 
+    /* Let's try not to verify this for every byte */
+    if (dev->1_wire_read_byte) {
+        for (idx = 0; idx < len; idx++) {
+            block[idx] = dev->1_wire_read_byte(dev);
+        }
+
+    } else {
+        for (idx = 0; idx < len; idx++) {
+            block[idx] = 1_wire_read_byte(dev);
+        }
+    }
 }
 
 /**
@@ -76,35 +115,49 @@ uint8_t
 void
 1_wire_write_block(struct 1_wire_dev *dev, const uint8_t *block, int len)
 {
+    if (dev->1_wire_write_block) {
+        return dev->1_wire_read_block(dev);
+    }
 
+    /* Let's try not to verify this for every byte */
+    if (dev->1_wire_write_byte) {
+        for (idx = 0; idx < len; idx++) {
+            dev->1_wire_write_byte(dev, block[idx]);
+        }
+
+    } else {
+        for (idx = 0; idx < len; idx++) {
+            1_wire_write_byte(dev, block[idx]);
+        }
+    }
 }
 
-/**
- *
- *
- */
-int
-1_wire_reset_select_slave(struct 1_wire_slave *slave)
-{
+/* /\** */
+/*  * */
+/*  * */
+/*  *\/ */
+/* int */
+/* 1_wire_reset_select_slave(struct 1_wire_slave *slave) */
+/* { */
 
-}
+/* } */
 
-/**
- *
- *
- */
-int
-1_wire_reset_resume_command(struct 1_wire_dev *dev)
-{
+/* /\** */
+/*  * */
+/*  * */
+/*  *\/ */
+/* int */
+/* 1_wire_reset_resume_command(struct 1_wire_dev *dev) */
+/* { */
 
-}
+/* } */
 
-/**
- *
- *
- */
-void
-1_wire_next_pullup(struct 1_wire_dev *dev, int)
-{
+/* /\** */
+/*  * */
+/*  * */
+/*  *\/ */
+/* void */
+/* 1_wire_next_pullup(struct 1_wire_dev *dev, int) */
+/* { */
 
-}
+/* } */
